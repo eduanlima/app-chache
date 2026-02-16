5574467208ab2b7cbf887f1b6885f739acdca921
eduanlima@github.com <eduandev@gmail.com>
2026-02-16 16:07:39 -0300

    Methiod to delete cache

---
+++ src/main/java/br/com/posterius/controller/CacheController.java
@@ -0,0 +1,21 @@
+package br.com.posterius.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.posterius.service.CacheService;
+
+@RestController
+@RequestMapping("/cache")
+public class CacheController {
+	@Autowired
+	private CacheService cacheService;
+	
+	@PostMapping
+	public void clear(@RequestParam("cacheName") String cacheName) {
+		cacheService.evictAllCacheValues(cacheName);
+	}
+}
---
+++ src/main/java/br/com/posterius/service/CacheService.java
@@ -0,0 +1,17 @@
+package br.com.posterius.service;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CacheService {
+	@Autowired
+	private CacheManager cacheManager;
+	
+	public void evictAllCacheValues(String cacheName) {
+		Objects.requireNonNull(cacheManager.getCache(cacheName)).clear();
+	}
+}
