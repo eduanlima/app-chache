a3eb7e1dfa527a171d8849a5a3fecefe86311625
eduanlima@github.com <eduandev@gmail.com>
2026-02-16 15:11:39 -0300

    Added cache

--- src/main/java/br/com/posterius/AppWithCacheApplication.java
+++ src/main/java/br/com/posterius/AppWithCacheApplication.java
@@ -2,8 +2,10 @@ package br.com.posterius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AppWithCacheApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/posterius/service/CompanyService.java
+++ src/main/java/br/com/posterius/service/CompanyService.java
@@ -3,6 +3,7 @@ package br.com.posterius.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import br.com.posterius.entity.CompanyEntity;
@@ -13,6 +14,7 @@ public class CompanyService {
 	@Autowired
 	private CompanyRepository companyRepository;
 	
+	@Cacheable("companies")
 	public List<CompanyEntity> findAll() {
 		return (List<CompanyEntity>) companyRepository.findAll();
 	}
